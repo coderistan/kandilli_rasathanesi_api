d4ff468142a3b7200b028b28af6b0c63c14da27f
coderistan <hardrevenge@hotmail.com>
2020-11-20 13:12:22 +0300

    cache eklendi

--- api/views.py
+++ api/views.py
@@ -4,10 +4,16 @@ from api.utils import *
 from api.serializers import DepremSerializer
 from rest_framework.views import APIView
 from rest_framework.pagination import LimitOffsetPagination
+from django.core.cache import cache
 
 class DepremView(APIView,LimitOffsetPagination):
     def get(self,request,format=None):
-        veriler = get_veriler()
+        if cache.get("deprem"):
+            veriler = cache.get("deprem")
+        else:
+            veriler = get_veriler()
+            cache.set("deprem",veriler,60*2)
+            
         results = self.paginate_queryset(veriler,request,self)
         s = DepremSerializer(results,many=True)
         return self.get_paginated_response(s.data)
